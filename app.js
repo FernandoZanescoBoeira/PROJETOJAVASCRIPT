896703d1343169b6ead779d5794d1b9511ef36e7
FernandoZanescoBoeira <129539259+FernandoZanescoBoeira@users.noreply.github.com>
2024-07-26 12:07:50 -0300

    ff

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao Jogo de Matemática Avançada');
+alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 10');
