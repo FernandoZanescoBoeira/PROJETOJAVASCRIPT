8237ede6a2fa9facc500af36cba2581b4e4ea3ea
FernandoZanescoBoeira <129539259+FernandoZanescoBoeira@users.noreply.github.com>
2024-07-26 11:57:07 -0300

    dfg

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
-// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-    } else {
-        alert('Você errou :(')
-    }
-    
+alert('Isso ai! Você descobriu o número secreto (5)');
+} else {
+alert('Você errou :(')
+}
